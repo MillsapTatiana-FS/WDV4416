166bb822d32ddc765f3aa849a0bcc6eb7cff75dd
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-10-29 09:31:41 -0500

    Adding requirements for assignment

---
+++ creditcard.test.js
@@ -0,0 +1,4 @@
+//Requirements for testing
+//TDD - create test for a module that will obscure the credit card number and return the last 4 digits
+//Test to make sure the function obscures the credit card number that are between the length of 12 and 16 digits 
+
